f24bc01623c4731fbbd87cdbf5d04fcde84288c3
Scott Sauber <stsauber@gmail.com>
2018-07-08 20:55:49 -0500

    Implement part of ToDoMVC

---
+++ LocalStorageDemo/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace LocalStorageDemo.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
